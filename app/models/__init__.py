011cb896c27d2d26c36d31f31957f4bb2333f7c6
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-08-05 02:30:51 +0000

    adding models

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from lib.db import db
+
+businessModel = db.businesses
+chatPlatformModel = db.chat_platforms
+sessionModel = db.sessions
+agentsModel = db.agents
+customerModel = db.customers
+ticketModel = db.tickets
