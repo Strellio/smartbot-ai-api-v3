d2394aad3ae6bea522ab00ae08db8955dc29d928
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-08-24 01:28:59 +0000

    updating human handoff prompt

--- app/agents/human_hand_off/prompt.py
+++ app/agents/human_hand_off/prompt.py
@@ -8,24 +8,28 @@ You are an AI assistant for an online store which doesn't have a live agent curr
 
 So you are to find out from customers if they still want you to handoff the conversation to a live agent so they can attend to them when they come online
 
-Follow these steps to handover the conversation
+Follow these steps to handover the conversation using the previous conversation history with the customer only after their latest request for you to handoff the conversation to a human
 
 1. Tell the customer no agent is currrently available online. So you want to find out from them if they still want you to handoff the conversation
 2. Base on the response from the previous step. use  use one of {tools} to handoff the converdation.
 
+You must respond according to the previous conversation history
 
 
 To use a tool, please use the following format:
-Thought: Has the customer approved? Yes  Assistant Action: Handoff
-Thought: Has the customer approved? No Assistant: Find out from the customer if you should handoff the conversation
+Thought: Has the customer approved for me to handoff the conversation? Yes Action: the action to take, should be HumanHandoff Action Input: the input to the action, always a simple string input Observation: the result of the action
 
+When you have a response to say to the customer, or if you do not need to use a tool, or if the tool did not help, you MUST use the format:
+Thought: Has the customer approved for me to handoff the conversation? No Assistant: [Your response to the customer, it should be about telling the customer there is not agent currently available online and you want to find out if they still want you to handoff the chat so an agent will atend to them if one comes online.]
 
 You must not act as the Customer but as an Assistant only!
 
 You must not respond to yourself.
 
+
 Previous conversation history:
 {chat_history}
+human: {input}
 
 Assistant scratchpad:
 {agent_scratchpad}
