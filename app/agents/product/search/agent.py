ec0f3ab4ef7612bc59594291dddeed9f06a2d638
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-09-07 04:13:19 +0000

    using openAI functions for product search

--- app/agents/product/search/agent.py
+++ app/agents/product/search/agent.py
@@ -11,6 +11,7 @@ from langchain.memory import ConversationBufferMemory
 
 from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 
+
 from langchain.agents.openai_functions_agent.base import OpenAIFunctionsAgent
 from langchain.schema.messages import SystemMessage
 from langchain.prompts import MessagesPlaceholder
@@ -39,11 +40,34 @@ class ProductKnowledgeBaseAgent(BaseModel):
         #     llm, tools, verbose=True, remember_intermediate_steps=False)
         system_message = SystemMessage(
             content=(
-                "Do your best to answer the questions. "
-                "Feel free to use any tools available to look up "
-                "You are not good at searching for products or have any knowlege about the product that we sell  so you must always use a tool for it"
-                "relevant information, only if neccessary"
-                "return the tool respponse to the customer as your response"
+                """
+Never forget you are a friendly and helpful shopping assistant that helps customers to:
+1. search for products
+2 get information about products
+3 sell products
+
+
+You are not good at performing these task:
+1. searching for products
+2. providing information about product
+3. sell products
+4. Providing product URL and other information about a product
+
+So for all the above task you must always use a tool for so you can perform them.
+
+Never tell the customer you are an AI shopping assistant so you can't perform them
+
+Don't tell them to find it on our website or any popular shopping website.
+
+ After using the tool you must finally respond to the customer according to the response from the tool.
+
+ If the tool did not return any response just tell the customer you did not find the product they are looking for.
+  
+ Don't try helping them as you are not good at helping customrs look for product.
+
+
+ 
+ """
 
             )
         )
@@ -54,15 +78,15 @@ class ProductKnowledgeBaseAgent(BaseModel):
 
         agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
 
-        product_knowledge_base_agent_with_tools = LLMSingleActionAgent(
-            output_parser=ProductKnowledgeBaseOutputParser(
-                business=business, customer=customer, chat_platform=chat_platform),
-            llm_chain=llm_chain,
-            stop=["\Observation:"],
-            allowed_tools=tool_names,
-            verbose=verbose,
-            max_iterations=max_iterations,
-        )
+        # product_knowledge_base_agent_with_tools = LLMSingleActionAgent(
+        #     output_parser=ProductKnowledgeBaseOutputParser(
+        #         business=business, customer=customer, chat_platform=chat_platform),
+        #     llm_chain=llm_chain,
+        #     stop=["\Observation:"],
+        #     allowed_tools=tool_names,
+        #     verbose=verbose,
+        #     max_iterations=max_iterations,
+        # )
 
         product_knowledge_base_agent = AgentExecutor.from_agent_and_tools(
             agent=agent, tools=tools, verbose=verbose, max_iterations=max_iterations, memory=getMemory(session_id=customer.get("_id"), db_name=business.get("account_name"),
--- app/agents/product/search/tools.py
+++ app/agents/product/search/tools.py
@@ -3,10 +3,12 @@ from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Redis, MongoDBAtlasVectorSearch
 from app.lib.db import atlasClient
-from langchain.tools import Tool
+from langchain.tools import Tool, StructuredTool
+
+from langchain.agents.agent_toolkits import create_retriever_tool
 
 
-from app.constant import ORDER_VECTORSTORE_COLLECTION_NAME, ORDER_VECTORSTORE_INDEX_NAME, PRODUCT_VECTORSTORE_COLLECTION_NAME, PRODUCT_VECTORSTORE_INDEX_NAME
+from app.constant import PRODUCT_VECTORSTORE_COLLECTION_NAME, PRODUCT_VECTORSTORE_INDEX_NAME
 from app.utils.memory import getMemory
 
 
@@ -32,23 +34,32 @@ def getProductKnowlegeBase(llm: ChatOpenAI, business, customer, verbose=False,  
     )
 
     product_doc_retriever = vectorstore.as_retriever()
-    knowledge_base = ConversationalRetrievalChain.from_llm(
-        llm=llm, chain_type="stuff", retriever=product_doc_retriever, verbose=verbose, memory=getMemory(session_id=customer.get("_id"), db_name=business.get("account_name"),
-                                                                                                        memory_key="chat_history", return_messages=True)
+
+    create_retriever_tool
+    knowledge_base = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=product_doc_retriever, verbose=verbose,
+        # memory=getMemory(session_id=customer.get("_id"), db_name=business.get("account_name"),
+        #                  memory_key="chat_history", return_messages=True)
     )
 
-    return knowledge_base
+    def parsing_multiplier(input):
+        if type(input) is list:
+            return knowledge_base.run(f"{''.join(input)}. Make sure you return the full information about the product")
+        else:
+            return knowledge_base.run(f"{input}. Make sure you return the full information about the product")
+
+    return parsing_multiplier
 
 
 def getTools(llm: ChatOpenAI, memory, business, customer, verbose=False, max_iterations=10, **kwargs):
     knowledge_base = getProductKnowlegeBase(
         llm=llm, business=business, customer=customer,  verbose=verbose)
     tools = [
-        Tool(
+        StructuredTool.from_function(
             name="ProductSearch",
-            func=knowledge_base.run,
+            func=knowledge_base,
             return_direct=False,
-            description="Searches and returns documents regarding the products we sell. The input to tool must always be a string or json string"
+            description="useful for when you need to answer questions about product information."
             # description="useful for when you need to answer questions about order information, getting status and update for orders",
         )
     ]
