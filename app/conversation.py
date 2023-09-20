341cf1428f5e46288b4d73e43880524574409cb6
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-09-20 22:26:01 +0000

    using gpt-4 only for team selection

--- app/conversation.py
+++ app/conversation.py
@@ -43,7 +43,7 @@ def conversation(input: Input):
     routing_memory = getMemory(session_id=input.sender, db_name=business.get("account_name"),
                                memory_key="chat_history", collection_name="chat_routing_history")
     sub_team_analyzer_chain = SubTeamAnalyzerChain.from_llm(
-        llm, verbose=True, memory=routing_memory)
+        llm=getLLM(model_name='gpt-4'), verbose=True, memory=routing_memory)
     sub_team_id = sub_team_analyzer_chain.run(input=input.message)
     print(sub_team_id)
     shop_assistant = ShopAssistant.init(
