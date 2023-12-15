60dbf0d2cef181efcc58301a081c211167c07719
wisdom0063 <kwartengwisdomug95@gmail.com>
2023-12-15 01:42:26 +0000

    update get agent

--- app/services/agents/get_business_agents.py
+++ app/services/agents/get_business_agents.py
@@ -5,5 +5,5 @@ from app.lib.db import agentsModel
 
 def getBusinessOnlineAgent(businessId: str):
     onlineAgents = agentsModel.find_one(
-        {"business": ObjectId(businessId), "availability_status": "available"})
+        {"business": ObjectId(businessId), "availability_status": "available", "status": "A"})
     return onlineAgents
